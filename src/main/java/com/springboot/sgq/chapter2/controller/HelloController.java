5aa4100e8a0418091bc4ac9e98df95ff0b1f4a9f
sunguangqi <sunguanqgi@qq.com>
2020-07-27 22:17:26 +0800

    HelloWorld

---
+++ src/main/java/com/springboot/sgq/chapter2/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.springboot.sgq.chapter2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String Hello(){
+        return "you";
+    }
+
+}
