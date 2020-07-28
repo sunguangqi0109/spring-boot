b6c77fffee766f6787b4724097f96b938db36533
sunguangqi <sunguanqgi@qq.com>
2020-07-28 17:04:57 +0800

    注解自动装配 1.普通方式 2.@Component 3.@Component  @ComponentScan

--- src/main/java/com/springboot/sgq/chapter2/controller/HelloController.java
+++ src/main/java/com/springboot/sgq/chapter2/controller/HelloController.java
@@ -4,12 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
     @RequestMapping("/hello")
     public String Hello(){
-        return "you";
+        return "a";
     }
 
 }
